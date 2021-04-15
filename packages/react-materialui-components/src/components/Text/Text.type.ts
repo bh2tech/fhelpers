b4cdc671ffabf000b9f540fea60a35591a490508
Pedro <pedrovoliveiracontato@gmail.com>
2021-04-15 15:54:36 -0300

    feat(fhelpers): add text component default

---
+++ packages/react-materialui-components/src/components/Text/Text.type.ts
@@ -0,0 +1,4 @@
+import { TypographyProps as TypographyPropsMaterial } from '@material-ui/core/Typography'
+import { IText } from '@fhelpers/types/'
+
+export type componentProps = IText & TypographyPropsMaterial
