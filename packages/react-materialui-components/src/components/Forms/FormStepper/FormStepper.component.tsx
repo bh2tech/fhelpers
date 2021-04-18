99261a7e254f53ef711090263eb347b909612fc8
Matheus Loureiro <mloureiropaz@gmail.com>
2021-04-17 23:51:20 -0300

    fix(react-materialui-components): add spacing to grid container inside form stepper

--- packages/react-materialui-components/src/components/Forms/FormStepper/FormStepper.component.tsx
+++ packages/react-materialui-components/src/components/Forms/FormStepper/FormStepper.component.tsx
@@ -116,7 +116,7 @@ export const FormStepper: React.FC<FormStepperProps> = ({
   }, [currentStep])
 
   return (
-    <Grid container>
+    <Grid container spacing={3}>
       <Grid item xs={12}>
         {children({
           isLoading,
